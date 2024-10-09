6f40584cb4de2dc763735145bd2ec1d8a9c39f22
Usman-837 <usmantufail837@gmail.com>
2024-10-09 11:35:43 +0500

    add heading and paragraph with tailwindcss style in banner section

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -8,6 +8,13 @@ const Banner = () => {
                 <div className="container">
                     <div className="mx-auto">
                         <img src={homeimage} alt="home image" className="w-60"/>
+                    </div>
+                    <div className="text-center">
+                        <h1 className="home__title">HAPPY TUMMY WITH TASTY BURGERS.</h1>
+                        <div className="separator mx-auto"></div>
+                        <p className="paragraph">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim neque quia amet nesciunt quibusdam harum, repudiandae ipsum nostrum qui reiciendis animi, voluptate ut quos tenetur voluptatem? Laborum vitae eum harum.
+                        </p>
                     </div>    
                 </div>
             </section>
