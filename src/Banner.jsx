5f5866b39e89b7ed5a75458f771e5ce1f0b57e07
Usman-837 <usmantufail837@gmail.com>
2024-10-09 11:59:39 +0500

    add icons in banner section

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -15,6 +15,23 @@ const Banner = () => {
                         <p className="paragraph">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim neque quia amet nesciunt quibusdam harum, repudiandae ipsum nostrum qui reiciendis animi, voluptate ut quos tenetur voluptatem? Laborum vitae eum harum.
                         </p>
+                        <div className="text-base flex items-center justify-center gap-4 py-10">
+                            <div className="text-center"> 
+                                <i className="fa-solid fa-utensils text-secondaryColor text-4xl"></i>
+                                <br/>
+                                Delicious
+                            </div>
+                            <div className="text-center"> 
+                                <i class="fa-solid fa-droplet text-secondaryColor text-4xl"></i>
+                                <br/>
+                                Fresh
+                            </div>
+                            <div className="text-center"> 
+                                <i class="fa-brands fa-envira text-secondaryColor text-4xl"></i>
+                                <br/>
+                                Organic
+                            </div>
+                        </div>
                     </div>    
                 </div>
             </section>
