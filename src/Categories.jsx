e661c2df1a6c0642867e9bc87fd9bf6efe35eac6
Muhammad Usman <usmantufail837@gmail.com>
2024-10-14 12:42:45 +0500

    add card 3 in categories section

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -27,6 +27,17 @@ const Categories = () => {
                             <p className="text-xs">Lorem ipsum dolor sit.</p>
                             <a href="#" className="text-secondaryColor cursor-pointer">Buy online</a>
                         </div>
+                    </div>
+                    {/* card 3 */}
+                       <div className="bg-greenColor flex py-3 rounded-lg overflow-hidden md:flex-1">
+                        <div className="basis-1/3 relative">
+                            <img src={burger1} alt="category image" className="absolute w-28 -button-4 left-4"/>
+                        </div>
+                        <div className="mb-2">
+                            <h1 className="card__title">Food</h1>
+                            <p className="text-xs">Lorem ipsum dolor sit.</p>
+                            <a href="#" className="text-secondaryColor cursor-pointer">Buy online</a>
+                        </div>
                     </div> 
                 </div>
             </section>
