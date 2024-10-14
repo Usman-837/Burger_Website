26fe697d50992a7d5327b3488b858bd1cf29d1f2
Muhammad Usman <usmantufail837@gmail.com>
2024-10-14 13:45:56 +0500

    add promo card 3 in promo section

--- src/Promo.jsx
+++ src/Promo.jsx
@@ -27,6 +27,16 @@ const Promo = () => {
                         <a href="#" className="text-xs text-secondaryColor">Buy online</a>
                     </div>
                 </div>
+                  {/* card 3 */}
+                  <div className="bg-primaryColorLight flex flex-col p-5 rounded-lg md:flex-row md:items-center lg:flex-row-reverse lg:flex-1">
+                    <img src={promoimg2} alt="promo image" className="w-40 mx-auto hover:animate-movingY md:mx-5"/>
+                    <div className="space-y-2 pt-5 md:pt-0">
+                        <p className="text-xs text-secondaryColor">Payday promo</p>
+                        <h3 className="card__title">GET A 10% DISCOUNT ON PAYDAY WEEK</h3>
+                        <p className="paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        <a href="#" className="text-xs text-secondaryColor">Buy online</a>
+                    </div>
+                </div>
             </div>
         </section>
         </>
