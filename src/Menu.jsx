2808505c9eba21329ec3b8fcac931e803b0460d2
Muhammad Usman <usmantufail837@gmail.com>
2024-10-15 16:18:27 +0500

    add state to manage active category and function to filter the menu based on the active category

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import menuimg1 from "./images/burger-1.png"
 import menuimg2 from "./images/burger-2.png"
 import menuimg3 from "./images/burger-3.png"
@@ -17,99 +17,121 @@ import beverageimg5 from "./images/beverage-5.png"
 
 
 const Menu = () => {
+    // State to manage active category
+    const [activeCategory, setActiveCategory] = useState("All");
 
     const burgermenu = [
         {
             title: "REGULAR BEEF BURGER",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$42.00",
-            image: menuimg1
+            image: menuimg1,
+            category: "Food",
         },
         {
             title: "REGULAR BEEF BURGER",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$40.00",
-            image: menuimg2
+            image: menuimg2,
+            category: "Food",
         },
         {
             title: "REGULAR BEEF BURGER",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$45.00",
-            image: menuimg3
+            image: menuimg3,
+            category: "Food",
         },
         {
             title: "REGULAR BEEF BURGER",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$48.00",
-            image: menuimg4
+            image: menuimg4,
+            category: "Food",
         },
         {
             title: "REGULAR BEEF BURGER",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: menuimg5
+            image: menuimg5,
+            category: "Food",
         },
         {
             title: "FRENCH FRIES",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: friesimg1
+            image: friesimg1,
+            category: "Snack",
         },
         {
             title: "FRENCH FRIES",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: friesimg2
+            image: friesimg2,
+            category: "Snack",
         },
         {
             title: "FRENCH FRIES",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: friesimg3
+            image: friesimg3,
+            category: "Snack",
         },
         {
             title: "FRENCH FRIES",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: friesimg4
+            image: friesimg4,
+            category: "Snack",
         },
         {
             title: "FRENCH FRIES",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: friesimg5
+            image: friesimg5,
+            category: "Snack",
         },
         {
             title: "SWEET SMOOTHIE",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: beverageimg1
+            image: beverageimg1,
+            category: "Beverage",
         },
         {
             title: "SWEET SMOOTHIE",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: beverageimg2
+            image: beverageimg2,
+            category: "Beverage",
         },
         {
             title: "SWEET SMOOTHIE",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: beverageimg3
+            image: beverageimg3,
+            category: "Beverage",
         },
         {
             title: "SWEET SMOOTHIE",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: beverageimg4
+            image: beverageimg4,
+            category: "Beverage",
         },
         {
             title: "SWEET SMOOTHIE",
             des: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
             price: "$35.00",
-            image: beverageimg5
+            image: beverageimg5,
+            category: "Beverage",
         },
-    ]
+    ];
+
+    // Function to filter the menu based on the active category
+    const filteredMenu = burgermenu.filter((item) =>
+        activeCategory === "All" ? true : item.category === activeCategory
+    );
 
     return (
         <>
@@ -121,21 +143,40 @@ const Menu = () => {
                         <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro accusantium totam dolores asperiores eaque ea architecto nostrum, delectus hic vitae iste. Nemo soluta, suscipit porro sit dignissimos libero magni in.</p>
                         <div className="tabs_wrap">
                             <ul className="flex flex-wrap justify-center gap-3 py-10">
-                                <li className="btn bg-primaryColorLight active">All</li>
-                                <li className="btn bg-primaryColorLight">Food</li>
-                                <li className="btn bg-primaryColorLight">Snack</li>
-                                <li className="btn bg-primaryColorLight">Beverage</li>
+                                <li
+                                    className={`btn bg-primaryColorLight ${activeCategory === "All" ? "active" : ""}`}
+                                    onClick={() => setActiveCategory("All")}
+                                >
+                                    All
+                                </li>
+                                <li
+                                    className={`btn bg-primaryColorLight ${activeCategory === "Food" ? "active" : ""}`}
+                                    onClick={() => setActiveCategory("Food")}
+                                >
+                                    Food
+                                </li>
+                                <li
+                                    className={`btn bg-primaryColorLight ${activeCategory === "Snack" ? "active" : ""}`}
+                                    onClick={() => setActiveCategory("Snack")}
+                                >
+                                    Snack
+                                </li>
+                                <li
+                                    className={`btn bg-primaryColorLight ${activeCategory === "Beverage" ? "active" : ""}`}
+                                    onClick={() => setActiveCategory("Beverage")}
+                                >
+                                    Beverage
+                                </li>
                             </ul>
                         </div>
                     </div>
                     <div>
                         <ul className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 lg:gap-12">
-                            {/* food */}
                             {
-                                burgermenu.map((data) => (
-                                    <li>
+                                filteredMenu.map((data, index) => (
+                                    <li key={index}>
                                         <div className="h-56 grid place-items-center bg-primaryColorLight rounded-3xl hover:bg-secondaryColor ease-linear duration-200 lg:h-40">
-                                            <img src={data.image} alt="food image" className="w-40 hover:scale-110 ease-linear duration-200 md:w-40 lg:w-24" />
+                                            <img src={data.image} alt={data.title} className="w-40 hover:scale-110 ease-linear duration-200 md:w-40 lg:w-24" />
                                         </div>
                                         <div className="pt-5">
                                             <div className="mb-2">
