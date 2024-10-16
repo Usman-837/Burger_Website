069a8a542c2da425e736d1f2ed12f130955e0ee7
Muhammad Usman <usmantufail837@gmail.com>
2024-10-16 17:52:38 +0500

    install react-slick and slick-carousel and add slider in customer review cards

--- src/Review.jsx
+++ src/Review.jsx
@@ -4,8 +4,47 @@ import customerimg2 from "./images/review-2.jpg"
 import customerimg3 from "./images/review-3.jpg"
 import customerimg4 from "./images/review-4.jpg"
 import customerimg5 from "./images/review-5.jpg"
+import Slider from "react-slick"
 
 const Review = () => {
+
+    var settings = {
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: "linear",
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        responsive: [
+          {
+            breakpoint: 10000,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+            },
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              initialSlide: 2,
+            },
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      };
 
     const customer_review = [
         {
@@ -42,19 +81,20 @@ const Review = () => {
 
     return (
         <>
-            <section id="menu">
+            <section id="review">
                 <div className="container">
                     <div className="max-w-md mx-auto text-center">
                         <h2 className="section__title">CUSTOMER REVIEW</h2>
                         <div className="separator mx-auto"></div>
                         <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro accusantium totam dolores asperiores eaque ea architecto nostrum, delectus hic vitae iste. Nemo soluta, suscipit porro sit dignissimos libero magni in.</p>
                     </div>
-                    <div>
+                    <div className="py-10">
                         <ul>
-                            {   
+                            <Slider {...settings}>
+                                {   
                                 customer_review.map((data, index) => (
                                     <li key={index}>
-                                        <div className="flex flex-col gap-5 bg-primaryColorLight rounded-lg p-6">
+                                        <div className="flex flex-col gap-5 bg-primaryColorLight rounded-lg p-6 mx-2">
                                             <p className="paragraph">{data.des}</p>
                                             <div className="flex items-center">
                                                 <img src={data.img} alt="review image" className="w-12 h-12 rounded-full"/>
@@ -67,7 +107,8 @@ const Review = () => {
                                         </div>
                                     </li>
                                 ))
-                            }                          
+                            }
+                            </Slider>                          
                         </ul>
                     </div>
                 </div>
